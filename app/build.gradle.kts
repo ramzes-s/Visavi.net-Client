12de8309971f4ed12b35188de4474c33085c369a
ramze <ramzes@smartoff.net>
2026-09-04 19:49:15 +0300

    chore: Bump version to v1.1.7 (11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ramzes.visavinet"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.6"
+        versionCode = 11
+        versionName = "1.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
